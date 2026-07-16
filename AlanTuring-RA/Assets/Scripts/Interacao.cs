1410e95ee63911b26056371f23ec6b26b068c44a
Gwaean <56048874+Gwaean@users.noreply.github.com>
2026-07-16 12:47:45 -0300

    adding stuff

---
+++ AlanTuring-RA/Assets/Scripts/Interacao.cs
@@ -0,0 +1,31 @@
+using Animancer;
+using FMODUnity;
+using UnityEngine;
+
+public class Mala : MonoBehaviour, IInteractable
+{
+    public AnimationClip idleAnimation;
+    public AnimationClip openAnimation;
+    [SerializeField] float reward;
+
+    private AnimancerComponent animancerComponent;
+    public EventReference openChest;
+
+    void Awake()
+    {
+        animancerComponent = GetComponent<AnimancerComponent>();
+    }
+
+    void Start()
+    {
+        if (idleAnimation != null) animancerComponent.Play(idleAnimation);
+    }
+
+    public void Interact()
+    {
+        GameManager.Instance.AddOxygen(reward);
+        RuntimeManager.PlayOneShot(openChest, transform.position);
+        GetComponent<BoxCollider2D>().enabled = false;
+        animancerComponent.Play(openAnimation);
+    }
+}
